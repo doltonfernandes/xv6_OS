3ee34e70816fe76bf516e664545b5157e0fb209e
doltonfernandes <dolton.fernandes@gmail.com>
2019-10-28 18:48:29 +0530

    Minor Changes

--- proc.h
+++ proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int ctime,etime,rtime;       // Assigment : Creation Time , End Time , Run Time of process
+  int ctime,etime,rtime;       // Assignment : Creation Time , End Time , Run Time of process
 };
 
 // Process memory is laid out contiguously, low addresses first:
