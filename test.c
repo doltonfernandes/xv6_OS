6c796ffee72f0706c04fe98c9b8a1f848074ff00
doltonfernandes <dolton.fernandes@gmail.com>
2019-10-29 13:59:24 +0530

    Some Changes

--- test.c
+++ test.c
@@ -6,5 +6,22 @@ main(void)
 {
 	// struct proc_stat *a=(struct proc_stat *)malloc(NPROC*sizeof(struct proc_stat));
 	// getpinfo();
+	int f=fork();
+	int a,b;
+	if(f<0)
+	{
+		exit();
+	}
+	if(f==0)
+	{
+		// Do something
+		sleep(5);
+		exit();
+	}
+	else
+	{
+		waitx(&a,&b);
+	}
+	printf(1,"%d %d\n",a,b);
 	exit();
 }
